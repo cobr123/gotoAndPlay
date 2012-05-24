a17b8240b9bdb31f1d9429004fa70a5d1d17527a
cobr123 <safebuster2@gmail.com>
2012-05-24 17:50:28 +0400

    the coin

--- gotoAndPlay/src/com/me/gotoandplay/GotoAndPlay.java
+++ gotoAndPlay/src/com/me/gotoandplay/GotoAndPlay.java
@@ -8,13 +8,16 @@ import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Rectangle;
 
 //http://www.gotoandplay.it/_articles/2007/02/game_tutorial_part1.php
 public class GotoAndPlay implements ApplicationListener {
 	Texture heroImage;
+	Texture coinImage;
 	Rectangle hero;
-	
+	Circle coin;
+
 	Camera camera;
 	SpriteBatch batch;
 	float power = 20;
@@ -24,21 +27,24 @@ public class GotoAndPlay implements ApplicationListener {
 	float gravity = 0.1f;
 	float thrust = 0.75f;
 	float wind = 0.1f;
-	
+
 	@Override
 	public void create() {
 		heroImage = new Texture(Gdx.files.internal("hero.png"));
-		
+		coinImage = new Texture(Gdx.files.internal("coin.png"));
+
 		camera = new OrthographicCamera();
 		((OrthographicCamera) camera).setToOrtho(false, 800, 480);
-		
+
 		batch = new SpriteBatch();
-		
+
 		hero = new Rectangle();
 		hero.x = 800 / 2 - 48 / 2;
 		hero.y = 400;
 		hero.width = 32;
 		hero.height = 32;
+
+		coin = new Circle(700, 400, 16);
 	}
 
 	@Override
@@ -49,32 +55,37 @@ public class GotoAndPlay implements ApplicationListener {
 	public void render() {
 		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
+
 		camera.update();
-		
+
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
+		batch.draw(coinImage, coin.x, coin.y);
 		batch.draw(heroImage, hero.x, hero.y);
 		batch.end();
-		
-		if(Gdx.input.isKeyPressed(Keys.LEFT)){
+
+		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
 			xspeed -= power * Gdx.graphics.getDeltaTime();
 		}
-		if(Gdx.input.isKeyPressed(Keys.RIGHT)){
+		if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
 			xspeed += power * Gdx.graphics.getDeltaTime();
 		}
-		if(Gdx.input.isKeyPressed(Keys.DOWN)){
+		if (Gdx.input.isKeyPressed(Keys.DOWN)) {
 			yspeed -= power * thrust * Gdx.graphics.getDeltaTime();
 		}
-		if(Gdx.input.isKeyPressed(Keys.UP)){
+		if (Gdx.input.isKeyPressed(Keys.UP)) {
 			yspeed += power * thrust * Gdx.graphics.getDeltaTime();
 		}
 		xspeed += wind;
 		xspeed *= friction;
 		yspeed -= gravity;
-		
+
 		hero.x += xspeed;
 		hero.y += yspeed;
+
+		if (hero.contains(coin.x + 8, coin.y + 8)) {
+			coin.x = (float) (Math.random() * 400 + 50);
+		}
 	}
 
 	@Override
